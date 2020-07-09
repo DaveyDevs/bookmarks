3b42183e3bd7e5666a60c8355cfb10aadf98103e
DaveyDevs <daveydevs@gmail.com>
2020-07-08 22:33:32 -0500

    it's working?!

--- script.js
+++ script.js
@@ -15,34 +15,7 @@ const LISTTEMPLATE = ITEMS.map((item) => {
   }</a><button class="delete-btn">Delete</button></li>`;
 });
 
-//Need to delete in two places? The live list and local storage? I figured out how to do the idnumber thing, let's see if that's helpful
-
-function deleteItem() {
-  console.log(LISTING);
-  // console.log(this.parentNode);
-  console.log(ITEMS); //local storage
-  // console.log([...LISTING.childNodes]); //live list
-
-  //Need to use childnodes to get to array of <li>s and [...LISTING.childNodes] to make it into an array we can work with
-
-  let filterList = [...LISTING.childNodes].filter((item) => {
-    return item !== this.parentNode;
-  });
-
-  localStorage.setItem("items", JSON.stringify(ITEMS));
-  console.log(filterList);
-
-  // console.log(this.parentNode.id);
-
-  // localStorage.setItem("items", JSON.stringify(filterList));
-
-  console.log(JSON.parse(localStorage.getItem("items")));
-}
-
 function populateItems() {
-  // console.log(LISTTEMPLATE.join(""));
-  // console.log(localStorage.items);
-  // console.log(ITEMS);
   LISTING.innerHTML = ITEMS.map((item) => {
     return `<li id="${item.idNum}"><a href="${item.LINK}" title="${
       item.DESC
@@ -66,7 +39,6 @@ function addItem(e) {
     DESC,
     idNum,
   });
-  // console.log(ITEMS);
 
   //should be setting the entire array
   localStorage.setItem("items", JSON.stringify(ITEMS));
@@ -76,6 +48,34 @@ function addItem(e) {
   this.reset(); //clears a form
 
   //Had to put this here too, so that the delete button works after adding items
+
+  const DELETEBTN = document.querySelectorAll(".delete-btn");
+
+  DELETEBTN.forEach((button) => {
+    button.addEventListener("click", deleteItem);
+  });
+}
+
+//Need to delete in two places? The live list and local storage? I figured out how to do the idnumber thing, let's see if that's helpful
+
+function deleteItem() {
+  localStorage.setItem("items", JSON.stringify(ITEMS));
+
+  let filteredItem = ITEMS.filter((item) => {
+    return item.idNum.toString() === this.parentNode.id;
+  });
+
+  console.log(filteredItem[0]);
+  console.log(ITEMS);
+  console.log(ITEMS.indexOf(filteredItem[0]));
+
+  let deleteIndex = ITEMS.indexOf(filteredItem[0]);
+
+  ITEMS.splice(deleteIndex, 1);
+
+  localStorage.setItem("items", JSON.stringify(ITEMS));
+
+  populateItems();
 
   const DELETEBTN = document.querySelectorAll(".delete-btn");
 
